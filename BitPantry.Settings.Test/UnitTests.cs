58067d95f625419c208a8db648a3d81ba9bf545d
Jeff Hewitt <jhewitt@credera.com>
2022-02-15 19:24:27 -0600

    Add project files.

---
+++ BitPantry.Settings.Test/UnitTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitPantry.Settings.Test
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ BitPantry.Settings/AppSettingsException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitPantry.Settings
+{
+    public class AppSettingsException : Exception
+    {
+        public AppSettingsException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
